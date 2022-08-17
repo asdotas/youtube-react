5e530ea2fea162753504e8ff9f291c5dd8a25e7c
astream <positive.thought1009@gmail.com>
2022-08-17 17:42:51 +0900

    get youtube api -7

---
+++ src/apis/index.js
@@ -0,0 +1,19 @@
+import axios from "axios"
+
+const KEY = 'AIzaSyCpR0yVeWfI3SEAib2XfST4LErhe2PYdRI';
+const youtube = axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3'
+})
+
+export const fetchPopularData = async () => {
+    return await youtube.get('videos', {
+        params: {
+            part: 'snippet',
+            maxResults: 40,
+            key:KEY,
+            regionCode: 'JP',
+            type: 'video',
+            chart: 'mostPopular'
+        }
+    })
+}
--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { fetchPopularData } from '../apis'
 import Layout from '../components/Layout/Layout'
+import { Store } from '../store'
 
 const Top = () => {
+  const { globalState, setGlobalState } = useContext(Store)
+
+  useEffect(() => {
+    fetchPopularData().then((res) => {
+      console.log('data', res)
+      setGlobalState({type: 'SET_POPULAR', payload: {popular: res.data.items}})
+      console.log(globalState);
+    })
+  }, [])
+
   return (
     <Layout>
         Top page
