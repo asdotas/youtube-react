80253501d3ac18ad867e44bff5e63e0a48e1a02a
astream <positive.thought1009@gmail.com>
2022-08-17 12:32:42 +0900

    create layout header components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Search from "./pages/Search";
+import Top from "./pages/Top";
+import Watch from "./pages/Watch";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Top />}></Route>
+        <Route path='/search' element={<Search />}></Route>
+        <Route path='/watch' element={<Watch />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+/*
+<Route path='/createpost' element={<CreatePost />}></Route>
+<Route exact path="/" component={Top} />
+<Route exact path="/search" component={Search} />
+<Route exact path="/watch" component={Watch} />
+*/
