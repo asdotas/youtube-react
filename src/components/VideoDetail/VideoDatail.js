bf48982432ca65cd6b8e4c90b70e09a128194f53
astream <positive.thought1009@gmail.com>
2022-08-18 21:38:42 +0900

    creating a video player -10

--- src/components/VideoDetail/VideoDatail.js
+++ src/components/VideoDetail/VideoDatail.js
@@ -2,27 +2,37 @@ import React, { useContext, useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 import { fetchSelectdData } from '../../apis'
 import { Store } from '../../store'
+import VideoPlay from '../VideoPlay/VideoPlay'
+import Style from './VideoDatail.module.scss'
+import Liinkify from 'react-linkify'
 
 const VideoDatail = () => {
     const { globalState, setGlobalState } = useContext(Store)
     const location = useLocation()
+
     const setSelectedVideo = async () => {
       const searchParams = new URLSearchParams(location.search)
       const id = searchParams.get('v')
       await fetchSelectdData(id).then((res) => {
         const item = res.data.items.shift()
-        setGlobalState({ type: 'SET_SELECTED', payload: {selected: item} })
+        setGlobalState({ type: 'SET_SELECTED', payload: {selected: item}})
       })
     }
+
     useEffect(() => {
       setSelectedVideo()
     }, [])
 
-  return (
-    <div>
-
+  return  globalState.selected && globalState.selected.id ? (
+    <div className={Style.wrap}>
+      <VideoPlay id={globalState.selected.id} />
+      <p>{globalState.selected.snippet.title}</p>
+      <hr />
+      <Liinkify>
+        <pre>{globalState.selected.snippet.description}</pre>
+      </Liinkify>
     </div>
-  )
+  ) : ( <span>no data</span>)
 }
 
 export default VideoDatail
